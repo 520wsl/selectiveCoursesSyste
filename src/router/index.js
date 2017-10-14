125ce360356e4f7bc566607a85b13a065a52fd34
wsl <395548460@qq.com>
2017-10-14 10:18:23 +0800

    路由添加懒加载功

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+const HelloWorld = r => require.ensure([], () => r(require('@/components/HelloWorld')), 'HelloWorld');
 
 export default new Router({
   routes: [
